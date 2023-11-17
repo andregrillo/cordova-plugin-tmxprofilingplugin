d99eedb2dc953b5ec14b123f83dc372f89f155f7
André Grillo <andregrillo@gmail.com>
2023-11-17 16:06:49 +0000

    Android init and doProfile implemented

--- src/android/TMXProfilingPlugin.java
+++ src/android/TMXProfilingPlugin.java
@@ -6,37 +6,29 @@ import com.lexisnexisrisk.threatmetrix.TMXEndNotifier;
 import com.lexisnexisrisk.threatmetrix.TMXProfiling;
 import com.lexisnexisrisk.threatmetrix.TMXProfilingHandle;
 import com.lexisnexisrisk.threatmetrix.TMXProfilingOptions;
+import com.lexisnexisrisk.threatmetrix.TMXScanEndNotifier;
+import com.lexisnexisrisk.threatmetrix.TMXStatusCode;
 import com.lexisnexisrisk.threatmetrix.tmxprofilingconnections.TMXProfilingConnections;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-
 import java.util.ArrayList;
 
 public class TMXProfilingPlugin extends CordovaPlugin {
+
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) {
         try {
-            // Parse the OrgId and FPServer from the args JSONArray
-            //Falta testar as variáveis de entrada
-            String orgId = args.getString(0);
-            String fpServer = args.getString(1);
-            //Faltam parâmetros aqui
-
-            // Use the parsed arguments to configure TMXProfiling
-            TMXProfilingConnections tmxConn = new TMXProfilingConnections();
-            TMXConfig config = new TMXConfig().setOrgId(orgId)
-                    .setFPServer(fpServer)
-                    .setContext(this.cordova.getActivity().getApplicationContext())
-                    .setDisableInitPackageScan(false)
-                    .setDisableProfilePackageScan(false)
-                    .setProfilingConnections(tmxConn);
-
-            TMXProfiling.getInstance().init(config);
-            doProfile();
-
-            //callbackContext.success(); // Signal success to the Cordova callback
+            if ("init".equals(action)) {
+                return init(args, callbackContext);
+            } else if ("doProfile".equals(action)) {
+                doProfile(callbackContext);
+                return true;
+            }  else {
+                callbackContext.error("Action not recognized.");
+                return false;
+            }
         } catch (JSONException e) {
             callbackContext.error("JSON argument parsing error: " + e.getMessage());
             return false;
@@ -44,44 +36,75 @@ public class TMXProfilingPlugin extends CordovaPlugin {
             callbackContext.error("Unexpected error: " + e.getMessage());
             return false;
         }
+    }
+
+    private boolean init(JSONArray args, CallbackContext callbackContext) throws JSONException {
+        // Check if the OrgId argument is provided and not null
+        if (args.isNull(0)) {
+            callbackContext.error("OrgId argument is missing or null.");
+            return false;
+        }
+
+        // Check if the FPServer argument is provided and not null
+        if (args.isNull(1)) {
+            callbackContext.error("FPServer argument is missing or null.");
+            return false;
+        }
+
+        // Parse the OrgId and FPServer from the args JSONArray
+        String orgId = args.getString(0);
+        String fpServer = args.getString(1);
+
+        // Validate the parsed arguments to ensure they are not empty
+        if (orgId.isEmpty()) {
+            callbackContext.error("OrgId argument is empty.");
+            return false;
+        }
+        if (fpServer.isEmpty()) {
+            callbackContext.error("FPServer argument is empty.");
+            return false;
+        }
+
+        // Use the parsed arguments to configure TMXProfiling
+        TMXProfilingConnections tmxConn = new TMXProfilingConnections();
+        TMXConfig config = new TMXConfig()
+                .setOrgId(orgId)
+                .setFPServer(fpServer)
+                .setContext(this.cordova.getActivity().getApplicationContext())
+                .setDisableInitPackageScan(false)
+                .setDisableProfilePackageScan(false)
+                .setRegisterForLocationServices(true)
+                .setDisableLocSerOnBatteryLow(true)
+                .setProfilingConnections(tmxConn);
+
+        TMXProfiling.getInstance().init(config);
+        callbackContext.success();
         return true;
     }
 
-    void doProfile() {
-        //Verificar o que deve ser passado no ArrayList
-       TMXProfilingOptions options = new TMXProfilingOptions().setCustomAttributes(new ArrayList<>());
-       // Fire off the profiling request. We could use a more complex request,
-       // but the minimum works fine for our purposes.
-       TMXProfilingHandle profilingHandle = TMXProfiling.getInstance().profile(options, new CompletionNotifier());
-   }
-}
+    private void doProfile(final CallbackContext callbackContext) {
+        TMXProfilingOptions options = new TMXProfilingOptions().setCustomAttributes(new ArrayList<String>());
 
-class CompletionNotifier implements TMXEndNotifier
-{
-    /**
-     * This gets called when the profiling has finished.
-     * We have to be careful here because we are not going to be called on the UI thread, and
-     * if we want to update UI elements we can only do it from the UI thread.
-     */
-    @Override
-    public void complete(TMXProfilingHandle.Result result)
-    {
-        //Get the session id to use in API call (AKA session query)
-        String m_sessionID = result.getSessionID();
-        Log.i("Plugin", "Profile completed with: " + result.getStatus().toString()+ " - " + result.getStatus().getDesc());
-        /*
-         * Profiling is complete, so login can proceed when the Login button is clicked.
-         */
-        //setProfileFinished(true);
+        TMXProfiling.getInstance().profile(options, new TMXEndNotifier() {
+            @Override
+            public void complete(TMXProfilingHandle.Result result) {
+                // Log the profiling result
+                Log.i("Plugin", "⭐️ Profiling is finished with result of " + result.getStatus() +
+                        " session id is " + result.getSessionID());
 
-        //CHAMAR O CALLBACK
+                // Check the status of the profiling
+                TMXStatusCode statusCode = result.getStatus();
+                if (statusCode == TMXStatusCode.TMX_OK) {
+                    // Profiling was successful, return the session ID
+                    callbackContext.success(result.getSessionID());
 
-//        /*
-//         * The Login button is clicked before the profiling is finished, therefore we should login
-//         * */
-//        if(isLoginClicked())
-//        {
-//            login();
-//        }
+                    // Start the scan packages
+                    TMXProfiling.getInstance().scanPackages();
+                } else {
+                    // Profiling failed, return the error description
+                    callbackContext.error("Profiling failed with status: " + statusCode.getDesc());
+                }
+            }
+        });
     }
 }
--- www/TMXProfilingPlugin.js
+++ www/TMXProfilingPlugin.js
@@ -1,20 +1,9 @@
-//Refazer tudo 
+var exec = require('cordova/exec');
 
-function TMXProfilingPlugin() {}
+exports.init = function (orgID, fpServer, success, error) {
+    exec(success, error, 'TMXProfilingPlugin', 'init', [orgID, fpServer]);
+};
 
-TMXProfilingPlugin.prototype.profile = function(message, duration, successCallback, errorCallback) {
-    var options = {};     
-    options.message = message;     
-    options.duration = duration;     
-    //Faltam variáveis aqui!
-    cordova.exec(successCallback, errorCallback, 'TMXProfilingPlugin', 'profile', [options]);
-}
-// Installation constructor that binds ToastyPlugin to window
-TMXProfilingPlugin.install = function() {
-    if (!window.plugins) 
-    {         window.plugins = {};     
-    }     
-    window.plugins.TMXProfilingPlugin = new TMXProfilingPlugin();
-    return window.plugins.TMXProfilingPlugin; 
-}; 
-cordova.addConstructor(TMXProfilingPlugin.install); 
+exports.doProfile = function (success, error) {
+    exec(success, error, 'TMXProfilingPlugin', 'doProfile', []);
+};
