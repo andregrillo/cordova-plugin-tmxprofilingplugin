674d2255f682974ad1785f9d22282e8bd4cee6d6
André Grillo <andregrillo@gmail.com>
2023-11-20 14:50:57 +0000

    Init optional parameters and Cancel Profile added

--- src/android/TMXProfilingPlugin.java
+++ src/android/TMXProfilingPlugin.java
@@ -14,6 +14,7 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class TMXProfilingPlugin extends CordovaPlugin {
 
@@ -25,8 +26,11 @@ public class TMXProfilingPlugin extends CordovaPlugin {
             } else if ("doProfile".equals(action)) {
                 doProfile(callbackContext);
                 return true;
-            }  else {
-                callbackContext.error("Action not recognized.");
+            } else if ("cancelProfile".equals(action)) {
+                cancelProfile(callbackContext);
+                return true;
+            } else {
+                callbackContext.error("Error: Action not recognized.");
                 return false;
             }
         } catch (JSONException e) {
@@ -41,13 +45,13 @@ public class TMXProfilingPlugin extends CordovaPlugin {
     private boolean init(JSONArray args, CallbackContext callbackContext) throws JSONException {
         // Check if the OrgId argument is provided and not null
         if (args.isNull(0)) {
-            callbackContext.error("OrgId argument is missing or null.");
+            callbackContext.error("Error: OrgId argument is missing or null.");
             return false;
         }
 
         // Check if the FPServer argument is provided and not null
         if (args.isNull(1)) {
-            callbackContext.error("FPServer argument is missing or null.");
+            callbackContext.error("Error: FPServer argument is missing or null.");
             return false;
         }
 
@@ -55,13 +59,20 @@ public class TMXProfilingPlugin extends CordovaPlugin {
         String orgId = args.getString(0);
         String fpServer = args.getString(1);
 
+        // Optional argument parsing
+        boolean registerForLocationServices = args.optBoolean(2, true); // Default to true if not provided
+        int screenOffTimeout = args.optInt(3, 180); // Default to 180 if not provided
+        boolean disableLocSerOnBatteryLow = args.optBoolean(4, false); // Default to false if not provided
+        int profileTimeout = args.optInt(5, 30); // Default to 30 if not provided
+        boolean disableNonfatalLogs = args.optBoolean(6, false); // Default to false if not provided
+
         // Validate the parsed arguments to ensure they are not empty
         if (orgId.isEmpty()) {
-            callbackContext.error("OrgId argument is empty.");
+            callbackContext.error("Error: OrgId argument is empty.");
             return false;
         }
         if (fpServer.isEmpty()) {
-            callbackContext.error("FPServer argument is empty.");
+            callbackContext.error("Error: FPServer argument is empty.");
             return false;
         }
 
@@ -73,9 +84,15 @@ public class TMXProfilingPlugin extends CordovaPlugin {
                 .setContext(this.cordova.getActivity().getApplicationContext())
                 .setDisableInitPackageScan(false)
                 .setDisableProfilePackageScan(false)
-                .setRegisterForLocationServices(true)
-                .setDisableLocSerOnBatteryLow(true)
+                .setRegisterForLocationServices(registerForLocationServices) //
+                .setScreenOffTimeout(screenOffTimeout, TimeUnit.SECONDS) //
+                .setDisableLocSerOnBatteryLow(disableLocSerOnBatteryLow) //
+                .setProfileTimeout(profileTimeout,TimeUnit.SECONDS)
                 .setProfilingConnections(tmxConn);
+
+        if (disableNonfatalLogs) {
+            config.disableNonfatalLogs();
+        }
 
         TMXProfiling.getInstance().init(config);
         callbackContext.success();
@@ -102,9 +119,32 @@ public class TMXProfilingPlugin extends CordovaPlugin {
                     TMXProfiling.getInstance().scanPackages();
                 } else {
                     // Profiling failed, return the error description
-                    callbackContext.error("Profiling failed with status: " + statusCode.getDesc());
+                    callbackContext.error("Error: Profiling failed with status " + statusCode.getDesc());
                 }
             }
         });
+    }
+
+    private void cancelProfile(final CallbackContext callbackContext) {
+        TMXProfilingHandle profilingHandle = TMXProfiling.getInstance().profile(new TMXEndNotifier(){
+            @Override
+            public void complete(TMXProfilingHandle.Result result)
+            {
+                Log.i("Plugin", "Profiling is finished with result of "+result.getStatus()+" session id is "+result.getSessionID());
+                // Check the status of the profiling
+                TMXStatusCode statusCode = result.getStatus();
+                if (statusCode == TMXStatusCode.TMX_OK) {
+                    // Profiling was successful, return the session ID
+                    callbackContext.success(result.getSessionID());
+
+                    // Start the scan packages
+                    TMXProfiling.getInstance().scanPackages();
+                } else {
+                    // Profiling failed, return the error description
+                    callbackContext.error("Error: Profiling failed with status " + statusCode.getDesc());
+                }
+            }
+        });
+        profilingHandle.cancel();
     }
 }
--- www/TMXProfilingPlugin.js
+++ www/TMXProfilingPlugin.js
@@ -1,9 +1,13 @@
 var exec = require('cordova/exec');
 
-exports.init = function (orgID, fpServer, success, error) {
-    exec(success, error, 'TMXProfilingPlugin', 'init', [orgID, fpServer]);
+exports.init = function (orgID, fpServer, registerForLocationServices, screenOffTimeout, disableLocSerOnBatteryLow, profileTimeout, disableNonfatalLogs, success, error) {
+    exec(success, error, 'TMXProfilingPlugin', 'init', [orgID, fpServer, registerForLocationServices, screenOffTimeout, disableLocSerOnBatteryLow, profileTimeout, disableNonfatalLogs]);
 };
 
 exports.doProfile = function (success, error) {
     exec(success, error, 'TMXProfilingPlugin', 'doProfile', []);
+};
+
+exports.cancelProfile = function (success, error) {
+    exec(success, error, 'TMXProfilingPlugin', 'cancelProfile', []);
 };
