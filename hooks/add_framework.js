ffb1141201ac226cbc20f37d9ad721bb52205842
André Grillo <andregrillo@gmail.com>
2023-12-21 11:53:15 +0000

    Hook added to inject SDK

--- hooks/add_framework.js
+++ hooks/add_framework.js
@@ -8,33 +8,25 @@ function getProjectName() {
     var name;
     parseString(config, function (err, result) {
         name = result.widget.name.toString();
-        const r = /\B\s+|\s+\B/g;  // Removes trailing and leading spaces
+        const r = /\B\s+|\s+\B/g;  //Removes trailing and leading spaces
         name = name.replace(r, '');
     });
-    name = name.replace(/\s/g, '\\ ');  // Escape spaces for file paths
+    // Escape spaces for file paths
+    name = name.replace(/\s/g, '\\ ');
     return name || null;
 }
 
 module.exports = function(context) {
-    var projectPath = path.join(context.opts.projectRoot, 'platforms/ios/' + getProjectName() + '.xcodeproj/project.pbxproj');
+    var projectPath = path.join('platforms/ios/' + getProjectName() + '.xcodeproj/project.pbxproj');
     var myProj = xcode.project(projectPath);
 
     myProj.parseSync();
 
-    // Add TMXProfilingConnections framework
-    var frameworkPath = path.join('plugins/TMXProfilingPlugin/src/ios/frameworks/TMXProfilingConnections.xcframework');
-    myProj.addFramework(frameworkPath, {customFramework: true, embed: true});
+    // Framework to add
+    var framework = path.join(context.opts.projectRoot, 'plugins/TMXProfilingPlugin/src/ios/frameworks/TMXProfilingConnections.xcframework');
 
-    // Modify build settings to ensure framework is set to "Embed & Sign"
-    var configurations = myProj.pbxXCBuildConfigurationSection();
-    for (var key in configurations) {
-        var config = configurations[key];
-        if (typeof config === 'object') {
-            var buildSettings = config.buildSettings;
-            buildSettings['CODE_SIGN_IDENTITY[sdk=iphoneos*]'] = '"iPhone Developer"';
-            buildSettings['LD_RUNPATH_SEARCH_PATHS'] = '"$(inherited) @executable_path/Frameworks"';
-        }
-    }
+    // Add framework to project
+    myProj.addFramework(framework, {customFramework: true, embed: true});
 
     // Write the modified project back to disk
     fs.writeFileSync(projectPath, myProj.writeSync());
